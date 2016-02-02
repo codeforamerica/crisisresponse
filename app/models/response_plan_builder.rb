b3b01063f7fb3ce3c9b1b9983ccd74c75e6f6c78
Grayson Wright <wright.grayson@gmail.com>
2016-02-02 13:41:48 -0800

    `fetch` data for fast failure if columns change

--- app/models/response_plan_builder.rb
+++ app/models/response_plan_builder.rb
@@ -3,40 +3,40 @@ class ResponsePlanBuilder
     data = data.to_h
 
     action_plan_steps = [
-      data["Step #1 (of action plan)"],
-      data["Step #2 (of action plan)"],
-      data["Step #3 (of action plan)"],
-      data["Step #4 (of action plan)"],
-      data["Step #5 (of action plan)"],
+      data.fetch("Step #1 (of action plan)"),
+      data.fetch("Step #2 (of action plan)"),
+      data.fetch("Step #3 (of action plan)"),
+      data.fetch("Step #4 (of action plan)"),
+      data.fetch("Step #5 (of action plan)"),
     ].compact
 
     past_encounters = (1..5).map do |n|
-      data["Descriptions of Past Encounter (##{n})"]
+      data.fetch("Descriptions of Past Encounter (##{n})")
     end.compact
 
     preparer = {
-      name: "What is your name?",
-      phone: "What is the best phone number to reach you?",
-      team: "With which team at SPD are you apart?",
+      name: data.fetch("What is your name?"),
+      phone: data.fetch("What is the best phone number to reach you?"),
+      team: data.fetch("Which team at SPD are you a part of?"),
     }
 
     ResponsePlan.new(
       action_plan_steps: action_plan_steps,
-      dob: data["Date of Birth"],
-      eyes: data["Eye Color"],
-      hair: data["Hair Color"],
-      height: data["Height"],
-      image: data["Image"],
-      license: data["License Number"],
-      name: data["Full Name"],
+      dob: data.fetch("Date of Birth"),
+      eyes: data.fetch("Eye Color"),
+      hair: data.fetch("Hair Color"),
+      height: data.fetch("Height"),
+      image: data.fetch("Image"),
+      license: data.fetch("License Number"),
+      name: data.fetch("Full Name"),
       past_encounters: past_encounters,
       preparer: preparer,
-      race: data["Race"],
-      sex: data["Gender"],
-      timestamp: data["Timestamp"].split.first,
-      type: data["What prompts you to write this plan?"],
-      veteran: data["Veteran?"],
-      weight: data["Weight"],
+      race: data.fetch("Race"),
+      sex: data.fetch("Gender"),
+      timestamp: data.fetch("Timestamp").split.first,
+      type: data.fetch("What prompts you to write this plan?"),
+      veteran: data.fetch("Veteran?"),
+      weight: data.fetch("Weight"),
     )
   end
 end
