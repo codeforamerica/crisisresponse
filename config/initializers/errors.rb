5a6509aad8e88af352fad22211772f415e10ba88
Grayson Wright <wright.grayson@gmail.com>
2016-05-11 19:06:09 -0700

    Remove all unused ActionMailer SMTP configuration
    Problem:
    
    We're using an unconventional method of sending email,
    based on a bash script that Toby wrote.
    
    Since we're not using the built-in Rails email framework,
    the configuration files for it are misleading to developers
    who might come to the project from a standard Rails background.
    
    Solution:
    
    Remove the unused configurations from the app.

--- config/initializers/errors.rb
+++ config/initializers/errors.rb
@@ -1,5 +1,4 @@
 require "net/http"
-require "net/smtp"
 
 # Example:
 #   begin
@@ -17,18 +16,3 @@ HTTP_ERRORS = [
   Net::ProtocolError,
   Timeout::Error,
 ]
-
-SMTP_SERVER_ERRORS = [
-  IOError,
-  Net::SMTPAuthenticationError,
-  Net::SMTPServerBusy,
-  Net::SMTPUnknownError,
-  Timeout::Error,
-]
-
-SMTP_CLIENT_ERRORS = [
-  Net::SMTPFatalError,
-  Net::SMTPSyntaxError,
-]
-
-SMTP_ERRORS = SMTP_SERVER_ERRORS + SMTP_CLIENT_ERRORS
