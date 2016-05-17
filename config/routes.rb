89e90a4d3c83cbc735fb15f02eb32ff0e9df6269
Grayson Wright <wright.grayson@gmail.com>
2016-05-16 19:15:53 -0700

    Remove Administrate from the app
    Problem:
    
    We're not at a point where we're ready to build admin dashboards.
    Even when we do build a dashboard for the CRT,
    it's likely to be more custom
    than what Administrate gives us out of the box.
    
    Solution:
    
    Remove Administrate until we have a use for it.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :response_plans
-    resources :response_strategies
-
-    root to: "response_plans#index"
-  end
-
   resources :response_plans, only: [:index, :show]
   resources :feedbacks, only: [:new, :create]
   resources :preferences, only: :create
