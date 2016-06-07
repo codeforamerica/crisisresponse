2e5f9928667986027d480ed93aa7dadf29965e75
Grayson Wright <wright.grayson@gmail.com>
2016-06-07 14:25:16 -0700

    Track better analytics data for page views
    Identify officers and response plans by their analytics tokens,
    not their database IDs.
    
    Remove the `KEEN_ENV` environment variable.
    We will differentiate environments
    by having multiple projects on Keen.io.
    
    Add [bowser](https://github.com/ded/bowser) to
    fetch information about the user's browser,
    which will let us differentiate mobile from MDT.

--- app/models/concerns/analytics.rb
+++ app/models/concerns/analytics.rb
@@ -1,7 +1,7 @@
 module Analytics
   def generate_analytics_token
     begin
-      self.analytics_token = SecureRandom.hex
+      self.analytics_token = SecureRandom.hex(4)
     end while self.class.exists?(analytics_token: analytics_token)
   end
 end
