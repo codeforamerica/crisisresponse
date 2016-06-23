fcfae5715877dba72bfb22be01609ffa7a744fc1
Grayson Wright <wright.grayson@gmail.com>
2016-06-23 09:30:45 -0700

    Reformat contacts into one section per contact
    Also increase the size of the dialer link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,11 @@
 module ApplicationHelper
-  def tel_to(text)
+  def tel_to(text, options = {})
     groups = text.to_s.scan(/(?:^\+)?\d+/)
-    link_to text, "tel:#{groups.join '-'}"
+
+    if block_given?
+      link_to("tel:#{groups.join '-'}", options) { yield }
+    else
+      link_to text, "tel:#{groups.join '-'}", options
+    end
   end
 end
