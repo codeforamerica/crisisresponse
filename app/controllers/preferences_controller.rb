e8007bc1966cd37450bfd0f69cbf0c00eda6616f
Grayson Wright <wright.grayson@gmail.com>
2016-06-30 22:12:36 -0700

    Switch between themes without a page refresh
    ## Problem:
    
    It takes a long time to switch between night and day themes,
    because it requires another request to the server.
    
    ## Solution:
    
    When the user clicks the button to change themes,
    immediately change the URL on the stylesheet link tag
    to the other theme's stylesheet URL.
    
    Send an AJAX post request to the server,
    so it can record the user's preference.
    
    See:
    http://api.rubyonrails.org/classes/ActionView/Helpers/AssetUrlHelper.html#method-i-path_to_stylesheet

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,16 +1,6 @@
 class PreferencesController < ApplicationController
   def create
-    session[:theme] = toggle(:day, :night, session[:theme])
+    session[:theme] = inactive_theme
     redirect_to :back
-  end
-
-  private
-
-  def toggle(first, second, current)
-    {
-      first => second,
-      second => first,
-      nil => second,
-    }.with_indifferent_access[current]
   end
 end
