db0fd282e1e98e8a89debc2ea545711d48674189
Grayson Wright <wright.grayson@gmail.com>
2016-08-09 22:52:44 -0700

    Make changing themes more consistent
    ## Problem
    
    With our fancy footwork for switching themes by swapping out the `href`
    attribute of our `link` tags,
    some officers reported that night mode would not persist
    after they left the current page.
    
    ## Solution
    
    Revert the `href`-swapping and AJAX solution for night mode,
    and go back to a regular page refresh.
    
    Officers will likely not be swapping back and forth so often
    that performance is a concern.
    
    Reverting the javascript also restores our tests to passing,
    where they had previously failed because of PhantomJS' inability to swap
    stylesheets on the fly.

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -3,4 +3,18 @@ class PreferencesController < ApplicationController
     session[:theme] = inactive_theme
     redirect_to :back
   end
+
+  private
+
+  def inactive_theme
+    toggle(:day, :night, theme)
+  end
+
+  def toggle(first, second, current)
+    {
+      first => second,
+      second => first,
+      nil => second,
+    }.with_indifferent_access[current]
+  end
 end
