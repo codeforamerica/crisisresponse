a293b4416b5793dc1631daedc1222ea4c41aefe5
Grayson Wright <wright.grayson@gmail.com>
2016-08-31 15:05:58 -0700

    Fix various deprecation warnings for Rails 5
    The deprecations in our app:
    
    ```
    DEPRECATION WARNING: `config.serve_static_files` is deprecated
    and will be removed in Rails 5.1.
    Please use `config.public_file_server.enabled = true` instead.
    
    DEPRECATION WARNING: `config.static_cache_control` is deprecated
    and will be removed in Rails 5.1.
    Please use
    `config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }`
    instead.
    
    DEPRECATION WARNING: ActiveRecord::Base.raise_in_transactional_callbacks= is deprecated,
    has no effect and will be removed without replacement.
    
    DEPRECATION WARNING: `redirect_to :back` is deprecated and will be removed from Rails 5.1.
    Please use `redirect_back(fallback_location: fallback_location)`
    where `fallback_location` represents the location to use
    if the request has no HTTP referer information.
    ```

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -1,7 +1,7 @@
 class PreferencesController < ApplicationController
   def create
     session[:theme] = inactive_theme
-    redirect_to :back
+    redirect_back(fallback_location: root_path)
   end
 
   private
