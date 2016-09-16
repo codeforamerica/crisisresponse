7556e963ba9f6f225dba996d544f1fcf2bc78dda
Grayson Wright <wright.grayson@gmail.com>
2016-09-16 15:46:06 -0700

    Index people based on visibility
    We are often querying and displaying only visible people.
    To speed up this query, we should index on the person's visibility.

--- db/migrate/20160913212716_add_visible_to_people.rb
+++ db/migrate/20160913212716_add_visible_to_people.rb
@@ -3,5 +3,6 @@
 class AddVisibleToPeople < ActiveRecord::Migration[5.0]
   def change
     add_column :people, :visible, :boolean, null: false, default: false
+    add_index :people, :visible
   end
 end
