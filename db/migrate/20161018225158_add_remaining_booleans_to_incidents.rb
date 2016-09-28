4725dc770b1dc43a608d8f1e74712798e4a60a17
Grayson Wright <wright.grayson@gmail.com>
2016-09-27 17:02:13 -0700

    Import remaining boolean attributes from RMS
    ## Problem
    
    There are many boolean attributes on crisis incident templates
    that we are not pulling into our application,
    but that would be useful for measuring in our application's metrics.
    
    ## Solution
    
    Import remaining boolean fields from the Crisis Incident Template

---
+++ db/migrate/20161018225158_add_remaining_booleans_to_incidents.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class AddRemainingBooleansToIncidents < ActiveRecord::Migration[5.0]
+  def change
+    change_table :rms_crisis_incidents do |t|
+      t.boolean :dicv
+      t.boolean :bodycam
+      t.boolean :cit_officer_requested
+      t.boolean :cit_officer_dispatched
+      t.boolean :cit_officer_arrived
+      t.boolean :behavior_other
+      t.boolean :weapon_knife
+      t.boolean :weapon_gun
+      t.boolean :weapon_other
+      t.boolean :handcuffs
+      t.boolean :reportable_force_used
+      t.boolean :unable_to_contact
+      t.boolean :cit_certified
+      t.boolean :supervisor_responded_to_scene
+      t.boolean :injuries
+    end
+  end
+end
