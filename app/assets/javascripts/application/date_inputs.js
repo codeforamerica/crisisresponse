13a840734be771860ba45b529a0820d6c443c59e
Grayson Wright <wright.grayson@gmail.com>
2016-10-17 18:05:15 -0700

    Mask safety concern date fields in form
    The date field masking process was only running on page load,
    so we need to re-run it whenever we add a date field to the page.
    Thankfully, it's idempotent.
    Running it multiple times on the same form field
    doesn't seem to have negative effects.

---
+++ app/assets/javascripts/application/date_inputs.js
@@ -0,0 +1,11 @@
+var mask_date_fields = function(){
+  var date_of_birth_fields = $('input[placeholder="mm-dd-yyyy"]');
+
+  date_of_birth_fields.
+     mask("99-99-9999", {
+     placeholder: "__-__-____",
+     autoclear: false
+   });
+}
+
+$(mask_date_fields);
