ba36d43db47aa8cd359ce550d8b9c6456970c19b
Grayson Wright <wright.grayson@gmail.com>
2016-10-18 10:41:45 -0700

    Save GO number without user formatting

--- spec/models/safety_concern_spec.rb
+++ spec/models/safety_concern_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe SafetyConcern, type: :model do
     it { should validate_presence_of :category }
     it { should validate_presence_of :title }
   end
+
+  describe "#go_number=" do
+    it "strips out all formatting" do
+      concern = SafetyConcern.new(go_number: "GO #2016-01010101")
+
+      expect(concern.go_number).to eq("201601010101")
+    end
+  end
 end
