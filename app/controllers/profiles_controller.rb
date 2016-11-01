43867543309eae35b403c9ac21ad67fb854b4318
Grayson Wright <wright.grayson@gmail.com>
2016-11-01 13:08:40 -0700

    Add pagination to profiles index
    There are about 5k profiles in the production app,
    so we cannot display them all on one page.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,8 +1,15 @@
 # frozen_string_literal: true
 
 class ProfilesController < ApplicationController
+  RECORDS_PER_PAGE = 12
+
   def index
-    @people = Person.all.includes(:response_plans, :visibilities, :reviews)
+    @people = Person.
+      all.
+      includes(:response_plans, :visibilities, :reviews).
+      page(params[:page]).
+      per(RECORDS_PER_PAGE)
+
     @due_for_review = @people.select(&:due_for_review?)
   end
 end
