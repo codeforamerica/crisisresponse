9ddd01a764d4e990a3e1919af17ad4e7535e2689
Grayson Wright <wright.grayson@gmail.com>
2016-11-01 13:15:15 -0700

    Add filtering by name to profiles page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,12 +4,22 @@ class ProfilesController < ApplicationController
   RECORDS_PER_PAGE = 12
 
   def index
-    @people = Person.
-      all.
+    @search = Search.new(search_params)
+
+    @people = @search.
+      close_matches.
       includes(:response_plans, :visibilities, :reviews).
       page(params[:page]).
       per(RECORDS_PER_PAGE)
 
     @due_for_review = @people.select(&:due_for_review?)
+  end
+
+  private
+
+  def search_params
+    params.require(:search).permit(:name)
+  rescue ActionController::ParameterMissing
+    {}
   end
 end
